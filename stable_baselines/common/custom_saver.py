2c8d99c94fe048866e63554788a4a4a4040e950d
Ku <t108598019@ntut.org.tw>
2020-09-06 21:11:24 +0800

    remove print in custom_saver

--- stable_baselines/common/custom_saver.py
+++ stable_baselines/common/custom_saver.py
@@ -15,7 +15,6 @@ class CustomSaver:
             with graph.as_default():
                 saver = tf.train.Saver()
                 saver.save(sess, os.path.join(self.ckpt_path, 'ckpt'), global_step=batch_num)
-                print ("Saved!" + str(batch_num))
 
     def restore(self, graph, sess, batch_num):
         if batch_num >= self.restore_batch:
@@ -23,17 +22,14 @@ class CustomSaver:
                 saver = tf.train.Saver()
                 try:
                     saver.restore(sess, os.path.join(self.ckpt_path, 'ckpt-' + str(batch_num)))
-                    print ("Restored!" + str(batch_num))
                 except:
                     pass
 
     def save_info(self, save_fn, data, batch_num, name):
         save_fn(os.path.join(self.info_path, name+'_batch'+str(batch_num)), data)
-        print ("Info Saved " + str(batch_num))
 
     def restore_info(self, load_fn, batch_num, name):
         if batch_num >= self.restore_batch:
-            print ("Info Restored " + str(batch_num))
             return load_fn(os.path.join(self.info_path, name+'_batch'+str(batch_num)))
         return(None, None)
 
