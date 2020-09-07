f40d9d88557b46e50b5b5ac1208d057d237a0411
Ku <t108598019@ntut.org.tw>
2020-09-07 13:43:38 +0800

    customsaver restore specified batch

--- stable_baselines/common/custom_saver.py
+++ stable_baselines/common/custom_saver.py
@@ -3,11 +3,12 @@ import tensorflow as tf
 
 
 class CustomSaver:
-    def __init__(self, ckpt_path, info_path, save_n_batch=0, restore_batch=0):
+    def __init__(self, ckpt_path, info_path, save_n_batch=0, restore_batch=0, only_batch=0):
         self.ckpt_path = ckpt_path
         self.info_path = info_path
         self.save_n_batch = save_n_batch
         self.restore_batch = restore_batch
+        self.only_batch = only_batch
         
     def save(self, graph, sess, batch_num, total_batches):
         save_freq = total_batches // self.save_n_batch
@@ -17,13 +18,16 @@ class CustomSaver:
                 saver.save(sess, os.path.join(self.ckpt_path, 'ckpt'), global_step=batch_num)
 
     def restore(self, graph, sess, batch_num):
-        if batch_num >= self.restore_batch:
-            with graph.as_default():
-                saver = tf.train.Saver()
-                try:
+        with graph.as_default():
+            saver = tf.train.Saver()
+            try:
+                if self.only_batch != 0 and batch_num == self.only_batch:
                     saver.restore(sess, os.path.join(self.ckpt_path, 'ckpt-' + str(batch_num)))
-                except:
-                    pass
+                elif self.only_batch == 0 and batch_num >= self.restore_batch:
+                    saver.restore(sess, os.path.join(self.ckpt_path, 'ckpt-' + str(batch_num)))
+                    
+            except:
+                pass
 
     def save_info(self, save_fn, data, batch_num, name):
         save_fn(os.path.join(self.info_path, name+'_batch'+str(batch_num)), data)
